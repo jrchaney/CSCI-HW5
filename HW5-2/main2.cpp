b095c99dcc2c71adc2df27f1b1af03fef2606ed8
jrchaney <111770024+jrchaney@users.noreply.github.com>
2022-09-21 15:43:27 +0000

    break with negative values

--- HW5-2/main2.cpp
+++ HW5-2/main2.cpp
@@ -4,14 +4,35 @@
 using namespace std;
 
 int main(){
+
+  int dataTemp;
+  int sumData = 0;
+  int counter = 0;
 // load input file
 ifstream inFile; // declare stream variable
 inFile.open("NumberData1.txt");
 
 // load all data in while loop
-while (!inFile.eof())
+while (inFile >> dataTemp)
   {
-  
+    //cout << dataTemp << endl;
+    // check to see if data is valid
+    if (dataTemp >= 0)
+    {
+      // update cumulative value of all data and number of data points loaded
+      sumData = sumData + dataTemp;
+      counter++;
+
+    }
+    else // the value is negative and we should exit
+    {
+      cout << "Negative value encountered of " << dataTemp << "." << endl;
+      cout << "Will not use the rest of the data... " << endl;
+      break;
+    
+    }
+    
+    
     
   }
   
